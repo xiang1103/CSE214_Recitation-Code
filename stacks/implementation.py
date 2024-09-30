a67a0b990827e8923dd6e0465516b101760e0de3
Xiang Liu <xiang.liu.1@stonybrook.edu>
2024-09-30 16:57:35 -0400

    New Unit Stacks/Queues

---
+++ stacks/implementation.py
@@ -0,0 +1,7 @@
+stack= []   #implement a stack 
+stack.append(1)     #implement push(), adds everything to the end 
+stack.append(2) 
+stack. pop()        #implement pop(), remove the last element of the stack 
+stack.pop(0)        #removeFirst of Queue 
+
+
