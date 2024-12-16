f082ea899a5899b8116ae7260a58aeda1f5fd29a
Xiang Liu <xiang.liu.1@stonybrook.edu>
2024-12-16 18:35:22 -0500

    Hello

--- C4Trees/tree.py
+++ C4Trees/tree.py
@@ -6,40 +6,40 @@ class Tree():
         self.left= None 
         self.right= None 
 
-def insert(root, key):
-    if root is None: 
-        root= Tree(key) 
-        return root 
-    if root.val==key:
-        return root 
-    elif key<root.val: 
-        root.left= insert(root.left, key)
-    else: 
-        root.right= insert(root.right,key)
+    def insert(root, key):
+        if root is None: 
+            root= Tree(key) 
+            return root 
+        if root.val==key:
+            return root 
+        elif key<root.val: 
+            root.left= self.insert(root.left, key)
+        else: 
+            root.right= self.insert(root.right,key)
 
-def preorder_trav(root):
-    if (root!=None):
-        print(root.val, end="->")
-        preorder_trav(root.left)
-        preorder_trav(root.right)
-    else:
-        return root 
+    def preorder_trav(root):
+        if (root!=None):
+            print(root.val, end="->")
+            preorder_trav(root.left)
+            preorder_trav(root.right)
+        else:
+            return root 
 
 
-def inorder (root):
-    if root is not None: 
-        inorder(root.left)
-        print(root.val, end="->")
-        inorder(root.right)
-    else:
-        return root 
-def postorder(root):
-    if root is not None: 
-        postorder(root.left)
-        postorder(root.right)
-        print(root.val, end="->")
-    else: 
-        return root 
+    def inorder (root):
+        if root is not None: 
+            inorder(root.left)
+            print(root.val, end="->")
+            inorder(root.right)
+        else:
+            return root 
+    def postorder(root):
+        if root is not None: 
+            postorder(root.left)
+            postorder(root.right)
+            print(root.val, end="->")
+        else: 
+            return root 
 
 def main(): 
     root= Tree(5) 
